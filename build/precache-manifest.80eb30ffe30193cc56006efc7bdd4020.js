0ef170b27b99ada2587c3dc6b27cd26541c8fd7c
ABDULRAHMAN abdullah ALKHUDAYRI <abdulrahman@ABDULRAHMANs-MacBook-Pro.local>
2020-02-03 16:15:08 +0300

    first publisher

---
+++ build/precache-manifest.80eb30ffe30193cc56006efc7bdd4020.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2141e6f53e21fe130d0a59acf0dc93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e48002870ea8353c49e6",
+    "url": "/static/css/main.a5cd2417.chunk.css"
+  },
+  {
+    "revision": "50aaf6381c48c8fa86db",
+    "url": "/static/js/0.1916373b.chunk.js"
+  },
+  {
+    "revision": "74374238c3a381c9e049",
+    "url": "/static/js/3.2d444d16.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/3.2d444d16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4207d793de491eb69ea7",
+    "url": "/static/js/4.8b373c71.chunk.js"
+  },
+  {
+    "revision": "098acd34f5f3037cb675",
+    "url": "/static/js/5.faaebba8.chunk.js"
+  },
+  {
+    "revision": "de6e80d89b0b21294c95",
+    "url": "/static/js/6.d7d88462.chunk.js"
+  },
+  {
+    "revision": "20f036c503e0136ff795",
+    "url": "/static/js/7.5d530697.chunk.js"
+  },
+  {
+    "revision": "bb2104bf6a86063d8aaf",
+    "url": "/static/js/8.5d2be013.chunk.js"
+  },
+  {
+    "revision": "e48002870ea8353c49e6",
+    "url": "/static/js/main.922a1432.chunk.js"
+  },
+  {
+    "revision": "c757328cedb5b051d6fb",
+    "url": "/static/js/runtime-main.17f3f120.js"
+  }
+]);
